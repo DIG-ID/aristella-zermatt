abf52aced2a752af808e905e12b9c08c3d086062
Daniel-Digid <daniel@dig.id>
2024-02-09 14:41:53 +0000

    d

--- template-parts/modules/voucher-bar.php
+++ template-parts/modules/voucher-bar.php
@@ -11,11 +11,11 @@
 		</div>
 
 		<div class="col-span-1 relative grid lg:block">
-			<a class="text-3xl uppercase text-beige text-center order-2 lg:order-1 -mt-5 lg:mt-0" href="/vouchershop/" target="_blank">
+			<a class="text-3xl uppercase text-beige text-center order-2 lg:order-1 -mt-5 lg:mt-0" href="<?php the_field( 'general_vouchershop_url', 'option' ); ?>" target="_blank">
 				<span class="font-primary_black block"><?php esc_html_e( 'GUTSCHEIN', 'aristella' ) ?></span>
 				<span class="font-primary_light block font-bold"><?php esc_html_e( 'SHOP', 'aristella' ) ?></span>
 			</a>
-			<a class="text-sm uppercase text-beige text-center block bg-no-repeat relative lg:absolute lg:top-[-3.3rem] left-1/2 -translate-x-1/2 w-[180px] h-[80px] order-1 lg:order-2" href="/vouchershop/" target="_blank">
+			<a class="text-sm uppercase text-beige text-center block bg-no-repeat relative lg:absolute lg:top-[-3.3rem] left-1/2 -translate-x-1/2 w-[180px] h-[80px] order-1 lg:order-2" href="<?php the_field( 'general_vouchershop_url', 'option' ); ?>" target="_blank">
 				<img class="absolute" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/vouchers-band.png" alt="vouchers" title="vouchers">
 				<span class="font-primary_light_cn_italic block z-30 relative pt-[6px]"><?php esc_html_e( 'ONLINE KAUFEN', 'aristella' ) ?></span>
 			</a>
