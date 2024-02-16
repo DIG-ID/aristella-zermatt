3a754a816fb3639bb60bb4c26cb551e7084296e2
Daniel-Digid <daniel@dig.id>
2024-02-16 14:57:17 +0000

    d

--- template-parts/pages/wellness/wellness.php
+++ template-parts/pages/wellness/wellness.php
@@ -4,8 +4,8 @@
         if( have_rows('wellness_list') ):
         while( have_rows('wellness_list') ) : the_row(); ?>
             <div class="col-span-1 md:col-span-8 xl:col-span-12 lg:mb-[100px] relative">
-                <div class="bg__wellness lg:absolute right-40 top-0 lg:w-[280px] lg:h-[220px] pt-16 lg:pt-4 px-5 pb-6">
-                    <h3 class="title-normal text-blue !text-[34px] uppercase mb-10"><?php the_sub_field( 'title' ); ?></h3>
+                <div class="bg__wellness lg:absolute right-40 top-0 lg:w-[320px] lg:h-[220px] pt-16 lg:pt-4 px-5 pb-6">
+                    <h3 class="title-normal text-blue !text-[34px] uppercase mb-12"><?php the_sub_field( 'title' ); ?></h3>
                     <p class="text-body text-beige"><?php the_sub_field( 'text' ); ?></p>
                 </div>
                 <?php 
