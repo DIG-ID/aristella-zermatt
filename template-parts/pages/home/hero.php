0be110c8472897953df7cde7b9cbcf58cd2acf08
Daniel-Digid <daniel@dig.id>
2026-03-27 18:05:44 +0000

    d

--- template-parts/pages/home/hero.php
+++ template-parts/pages/home/hero.php
@@ -3,9 +3,9 @@
 		<div class="col-span-1 md:col-span-8 xl:col-span-12">
 			<div class="hero__content absolute w-full z-30 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center px-8">
 				<h2 class="font-primary_bold uppercase text-beige text-[28px] sm:text-[28px] lg:text-[56px] text-shadow-aris mb-5"><?php the_field( 'hero_title' ); ?></h2>
-				<?php 
+				<?php
 				$link = get_field('hero_button_link');
-				if( $link ): 
+				if( $link ):
 					$link_url = $link['url'];
 					$link_title = $link['title'];
 					$link_target = $link['target'] ? $link['target'] : '_self';
@@ -14,10 +14,10 @@
 				<?php endif; ?>
 			</div>
 			<div class="hero__content absolute w-full z-30 left-1/2 -translate-x-1/2 bottom-0 text-center max-h-24 hidden xl:block">
-                <a href="#section-intro" class="inline-block w-[30px] md:w-[60px]">
-                    <svg class="arrowsScroll animatedArrow"><path class="a1" d="M0 0 L30 32 L60 0"></path></svg>
-                </a>
-            </div>
+				<a href="#section-intro" class="inline-block w-[30px] md:w-[60px]">
+					<svg class="arrowsScroll animatedArrow"><path class="a1" d="M0 0 L30 32 L60 0"></path></svg>
+				</a>
+			</div>
 			<?php
 			$slider = get_field( 'hero_hero_slider' );
 			if ( $slider ) :
@@ -27,10 +27,16 @@
 						<?php
 						$counter = 0;
 						foreach ( $slider as $slider_id ) :
+							$attrs = array(
+								'class'   => 'slide-bg slide-bg--' . esc_attr( $counter ) . ' w-full object-cover object-top h-[530px] md:h-[750px] xl:h-[760px]',
+								'loading' => $counter === 0 ? 'eager' : 'lazy',
+							);
+							if ( $counter === 0 ) {
+								$attrs['fetchpriority'] = 'high';
+							}
 							?>
 							<div class="swiper-slide">
-								<div class="slide-bg slide-bg--<?php echo esc_attr( $counter ); ?> w-full min-h-[530px] md:min-h-[750px] xl:min-h-[760px] bg-cover<?php echo $counter === 0 ? ' rocket-no-lazy' : ''; ?>" style="background-image: url('<?php echo esc_url( wp_get_attachment_image_url( $slider_id, 'full' ) ); ?>');background-size: cover; background-repeat: no-repeat;"></div>
-								<?php /*echo wp_get_attachment_image( $slider_id, 'full', false, array( 'class' => 'w-full object-cover min-h-[530px] md:min-h-[750px] xl:min-h-[760px]', 'loading' => 'lazy' ) );*/ ?>
+								<?php echo wp_get_attachment_image( $slider_id, 'full', false, $attrs ); ?>
 							</div>
 							<?php
 							$counter++;
@@ -44,6 +50,6 @@
 			?>
 		</div>
 		<div class="swiper-button-next swiper-button-next-1"></div>
-        <div class="swiper-button-prev swiper-button-prev-1"></div>
+		<div class="swiper-button-prev swiper-button-prev-1"></div>
 	</div>
 </section>
