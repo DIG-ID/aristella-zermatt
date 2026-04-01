f414c79cd48dd7c518e62d7d19c957a60ac6a079
Daniel-Digid <daniel@dig.id>
2026-04-01 13:17:09 +0100

    Update header.php

--- template-parts/header.php
+++ template-parts/header.php
@@ -3,17 +3,20 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="facebook-domain-verification" content="12836mnj7fikqkczxez5xa1itsgrax" />
-		<!-- Google tag (gtag.js) -->
-			<script async src="https://www.googletagmanager.com/gtag/js?id=G-V9DWWVCDLH">
-			</script>
-			<script> 
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-V9DWWVCDLH');
-			</script>
-		<!-- End Google Analytics -->
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TQFHKLT');</script>
+		<!-- End Google Tag Manager -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<?php wp_head(); ?>
 	</head>
 
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQFHKLT"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 		<?php do_action( 'wp_body_open' ); ?>
