ce175581e306b1bb217f527ccc51ea7389dfb976
Daniel-Digid <daniel@dig.id>
2026-04-07 12:47:59 +0100

    Update content.php

--- template-parts/pages/vouchershop/content.php
+++ template-parts/pages/vouchershop/content.php
@@ -1,13 +1,14 @@
 <div id="myIncertIframe"></div>
-<script type="text/javascript" src="https://myincert.com/public/api/incertClient.js"></script>
-<script type="text/javascript">
-var iframe_src = "https://aristella.traumgutscheine.com/start.php?incertframe=new";
-</script>
-<script type="text/javascript">
+<script type="text/plain" data-cookieconsent="statistics">
 (function(){
-var frameContainerID = 'myIncertIframe',
-landingPageURL = iframe_src,
-trackingCode = 'UA-123456789';
-incertIframe(frameContainerID, landingPageURL, trackingCode);
+    var script = document.createElement('script');
+    script.src = 'https://myincert.com/public/api/incertClient.js';
+    script.onload = function() {
+        var frameContainerID = 'myIncertIframe',
+            landingPageURL = 'https://aristella.traumgutscheine.com/start.php?incertframe=new',
+            trackingCode = 'UA-123456789';
+        incertIframe(frameContainerID, landingPageURL, trackingCode);
+    };
+    document.head.appendChild(script);
 })();
 </script>
